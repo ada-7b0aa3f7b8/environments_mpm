090a84f6f61ff92c3245c9db4c6ec01eff99d996
ada-7b0aa3f7b8 <pa825@ic.ac.uk>
2025-10-06 11:46:22 +0100

    updated env files and added a .gitignore file

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,22 @@
+from typing import List
+import pandas as pd
 import numpy as np
 
 
-__all__ = ['rand_array']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve', 'panda_dataframe']
+
+
+def smooth_image(a, sigma=1):
+    return gaussian_filter(a, sigma=sigma)
 
 
 def rand_array(shape):
     return np.random.rand(*shape)
+
+
+def my_mat_solve(A, b):
+    return A.inv()*b
+
+
+def panda_dataframe(name: str, nums: List[int]):
+    return pd.DataFrame({name: nums})
